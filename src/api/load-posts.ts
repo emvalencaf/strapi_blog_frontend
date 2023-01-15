8a1647bcc584644514191b2566d8d6e6a7a0b6c2
Edson Valença Filho <edsonmvf@gmail.com>
2023-01-15 19:56:11 -0300

    Refactored types in api

--- src/api/load-posts.ts
+++ src/api/load-posts.ts
@@ -2,51 +2,11 @@ import { request } from "graphql-request";
 import config from "../config";
 import { GRAPHQL_QUERIES } from "../graphql/queries";
 
-// type
-import { PostStrapi } from "../shared-types/post-strapi";
-import { SettingsStrapi } from "../shared-types/settings-strapi";
-import { mapDataGraphQL, PostsFromDataGraphQLAttributesProps, SettingFromDataGraphQLAttributesProps } from "./map-lod-posts";
-type StringFilterInput = {
-	and?: string;
-	or?: string;
-	not?: StringFilterInput;
-	eq?: string;
-	ne?: string;
-	startsWith?: string;
-	endsWith?: string;
-	contains?: string;
-	notContains?: string;
-	containsi?: string;
-	notContainsi?: string;
-	gt?: string;
-	gte?: string;
-	lt?: string;
-	lte?: string;
-	null?: boolean;
-	notNull?: boolean;
-	in?: string;
-	notIn?: string;
-	between?: string;
-};
-export type ResponseLoadPosts = {
-	posts: PostStrapi[];
-	settings: SettingsStrapi;
-};
-export type ResponseRequest = {
-	posts: PostsFromDataGraphQLAttributesProps;
-	setting: SettingFromDataGraphQLAttributesProps;
-};
+// imported functions
+import { mapDataGraphQL } from "./map-lod-posts";
 
-export type LoadPostsVariables = {
-	categorySlug?: StringFilterInput;
-	postSlug?: StringFilterInput;
-	postSearch?: StringFilterInput;
-	authorSlug?: StringFilterInput;
-	tagSlug?: StringFilterInput;
-	sort?: string;
-	start?: number;
-	limit?: number;
-};
+// type
+import { LoadPostsVariables, ResponseLoadPosts, ResponseRequest } from "./types";
 
 export const loadPosts = async (
 	variables: LoadPostsVariables = {}
---
+++ src/api/types.ts
@@ -0,0 +1,129 @@
+import { MenuPropsLink } from "../components/Menu";
+import { PostStrapi } from "../shared-types/post-strapi";
+import { SettingsStrapi } from "../shared-types/settings-strapi";
+export type StringFilterInput = {
+	and?: string;
+	or?: string;
+	not?: StringFilterInput;
+	eq?: string;
+	ne?: string;
+	startsWith?: string;
+	endsWith?: string;
+	contains?: string;
+	notContains?: string;
+	containsi?: string;
+	notContainsi?: string;
+	gt?: string;
+	gte?: string;
+	lt?: string;
+	lte?: string;
+	null?: boolean;
+	notNull?: boolean;
+	in?: string;
+	notIn?: string;
+	between?: string;
+};
+export type ResponseLoadPosts = {
+	posts: PostStrapi[];
+	settings: SettingsStrapi;
+};
+export type ResponseRequest = {
+	posts: PostsFromDataGraphQLAttributesProps;
+	setting: SettingFromDataGraphQLAttributesProps;
+};
+
+export type LoadPostsVariables = {
+	categorySlug?: StringFilterInput;
+	postSlug?: StringFilterInput;
+	postSearch?: StringFilterInput;
+	authorSlug?: StringFilterInput;
+	tagSlug?: StringFilterInput;
+	sort?: string;
+	start?: number;
+	limit?: number;
+};
+
+type CoverFromDataGraphQLAttributesProps = {
+	data: {
+		id: string;
+		attributes: {
+			alternativeText: string;
+			url: string;
+		};
+	};
+};
+
+type LogoFromDataGraphQLAttributesProps = {
+	data: {
+		id: string;
+		attributes: {
+			alternativeText: string;
+			url: string;
+		};
+	};
+};
+
+type CategoriesFromDataGraphQLAttributesProps = {
+	data: {
+		id: string;
+		attributes: {
+			displayName: string;
+			slug: string;
+		};
+	}[];
+};
+
+type TagsFromDataGraphQLAttributesProps = {
+	data: {
+		id: string;
+		attributes: {
+			displayName: string;
+			slug: string;
+		};
+	}[];
+};
+
+type AuthorFromDataGraphQLAttributesProps = {
+	data: {
+		id: string;
+		attributes: {
+			displayName: string;
+			slug: string;
+		};
+	};
+};
+
+type PostsFromDataGraphQLAttributesProps = {
+	data: {
+		id: string;
+		attributes: {
+			title: string;
+			slug: string;
+			excerpt: string;
+			content: string;
+			displayName: string;
+			allowComments: boolean;
+			logo: LogoFromDataGraphQLAttributesProps;
+			cover: CoverFromDataGraphQLAttributesProps;
+			tags: TagsFromDataGraphQLAttributesProps;
+			categories: CategoriesFromDataGraphQLAttributesProps;
+			author: AuthorFromDataGraphQLAttributesProps;
+			createdAt: string;
+		};
+	}[];
+};
+
+type SettingFromDataGraphQLAttributesProps = {
+	data: {
+		id: string;
+		attributes: {
+			blogName: string;
+			blogDescription: string;
+			menuLink: MenuPropsLink[];
+			logo: LogoFromDataGraphQLAttributesProps;
+			footer: string;
+		};
+	};
+};
+
+export type MapDataGraphQL = (dataGraphQL:ResponseRequest) => ResponseLoadPosts;
